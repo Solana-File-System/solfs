f9728c36b1f6ac838cfcbf849476929e50ec4a2f
Olanrewaju Oladunjoye <olanrewaju.oladunjoye@web-veistamo.fi>
2025-01-21 19:46:22 +0200

    map custom errors to program error for runtime compatibility

--- dataStore/src/error.rs
+++ dataStore/src/error.rs
@@ -3,33 +3,37 @@ use solana_program::program_error::ProgramError;
 use thiserror::Error;
 
 #[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
-pub enum DataStoreError { 
+pub enum DataStoreError {
     #[error("Instruction is not implemented")]
     NotImplemented,
-    #[error("Account should be writeable")]
+    #[error("Account should be writable")]
     NotWriteable,
-    #[error("Account should not have zero length data")]
+    #[error("Account should not have zero-length data")]
     NoAccountLength,
     #[error("Account data length is invalid (non-zero data)")]
     NonZeroData,
-    #[error("Account should be a signer)")]
+    #[error("Account should be a signer")]
     NotSigner,
-    #[error("Account should be valid system program")]
+    #[error("Account should be a valid system program")]
     InvalidSystemProgram,
-    #[error("Account should be valid owner of data store account")]
+    #[error("Account should be a valid owner of the data store account")]
     InvalidAuthority,
-    #[error("Account should be PDA of data account")]
+    #[error("Account should be a valid PDA of the data account")]
     InvalidPDA,
-    #[error("Cannot reinitialize previously initialized data store account")]
+    #[error("Cannot reinitialize a previously initialized data store account")]
     AlreadyInitialized,
     #[error("Data account should be initialized")]
     NotInitialized,
-    #[error("Cannot update previously finalized data store account")]
+    #[error("Cannot update a previously finalized data store account")]
     AlreadyFinalized,
     #[error("Operation overflowed")]
     Overflow,
     #[error("Data account should have sufficient space")]
     InsufficientSpace,
+    #[error("Invalid data type for verification")]
+    InvalidDataType,
+    #[error("Data verification failed")]
+    DataVerificationFailed,
 }
 
 impl From<DataStoreError> for ProgramError {

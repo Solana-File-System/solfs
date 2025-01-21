d7bc9641f42576e3d0db5608cf5b3570ddd06664
Olanrewaju Oladunjoye <olanrewaju.oladunjoye@web-veistamo.fi>
2025-01-21 19:44:01 +0200

    improve and optimize state management for

--- dataStore/src/state.rs
+++ dataStore/src/state.rs
@@ -30,7 +30,7 @@ pub enum SerializationStatusOption {
     FAILED,
 }
 
-/// Verfies that the data conforms to the data_type
+/// Verifies that the data conforms to the data_type
 pub fn verify_data(data: &[u8], data_type: DataStoreTypeOption) -> SerializationStatusOption {
     if data.is_empty() || data_type == DataStoreTypeOption::CUSTOM {
         return SerializationStatusOption::UNVERIFIED;
@@ -42,15 +42,13 @@ pub fn verify_data(data: &[u8], data_type: DataStoreTypeOption) -> Serialization
                 Err(_) => SerializationStatusOption::FAILED,
             }
         }
-        DataStoreTypeOption::IMG => SerializationStatusOption::VERIFIED,
-        DataStoreTypeOption::HTML => SerializationStatusOption::VERIFIED,
-        DataStoreTypeOption::PDF => SerializationStatusOption::VERIFIED,
+        _ => SerializationStatusOption::VERIFIED,
     }
 }
 
 #[derive(Clone, Debug, BorshSerialize, BorshDeserialize, ShankAccount)]
-pub struct DataStoreAccountMetadata{
-    data_status: DataStoreStatusOption,
+pub struct DataStoreAccountMetadata {
+    data_status: DataStoreState,
     serialization_status: SerializationStatusOption,
     authority: Pubkey,
     is_dynamic: bool,
@@ -61,7 +59,7 @@ pub struct DataStoreAccountMetadata{
 impl DataStoreAccountMetadata {
     pub fn new(authority: Pubkey, data_type: DataStoreTypeOption, bump_seed: u8) -> Self {
         Self {
-            data_status: DataStoreStatusOption::Uninitialized,
+            data_status: DataStoreState::Uninitialized,
             serialization_status: SerializationStatusOption::UNVERIFIED,
             authority,
             is_dynamic: false,
@@ -69,85 +67,44 @@ impl DataStoreAccountMetadata {
             bump_seed,
         }
     }
-}
 
-/// Get the Data Status
-pub fn data_status(&self) -> DataStoreStatusOption {
-    &self.data_status
-}
+    pub fn data_status(&self) -> &DataStoreState {
+        &self.data_status
+    }
 
-/// set the Data Status
-pub fn set_data_status(&mut self, data_status: DataStoreStatusOption) {
-    self.data_status = data_status;
-}
+    pub fn set_data_status(&mut self, data_status: DataStoreState) {
+        self.data_status = data_status;
+    }
 
-/// get serialization status
-pub fn serialization_status(&self) -> SerializationStatusOption {
-    &self.serialization_status
-}
+    pub fn serialization_status(&self) -> &SerializationStatusOption {
+        &self.serialization_status
+    }
 
-/// set serialization status
-pub fn set_serialization_status(&mut self, serialization_status: SerializationStatusOption) {
-    self.serialization_status = serialization_status;
-}
+    pub fn set_serialization_status(&mut self, serialization_status: SerializationStatusOption) {
+        self.serialization_status = serialization_status;
+    }
 
-/// get the authority
-pub fn authority(&self) -> &Pubkey {
-    &self.authority
-}
+    pub fn authority(&self) -> &Pubkey {
+        &self.authority
+    }
 
-/// set the authority
-pub fn set_authority(&mut self, authority: Pubkey) {
-    self.authority = authority;
-}
+    pub fn set_authority(&mut self, authority: Pubkey) {
+        self.authority = authority;
+    }
 
-/// get the is_dynamic
-pub fn is_dynamic(&self) -> bool {
-    &self.is_dynamic
-}
+    pub fn is_dynamic(&self) -> bool {
+        self.is_dynamic
+    }
 
-/// get current data version
-pub fn version(&self) -> u8 {
-    self.data_version
+    pub fn data_type(&self) -> &DataStoreTypeOption {
+        &self.data_type
+    }
 
-}
+    pub fn set_data_type(&mut self, data_type: DataStoreTypeOption) {
+        self.data_type = data_type;
+    }
 
-/// get the data type
-pub fn data_type(&self) -> &DataStoreTypeOption {
-    &self.data_type
-}
-
-/// set the data type
-pub fn set_data_type(&mut self, data_type: DataStoreTypeOption) {
-    self.data_type = data_type;
-}
-
-/// get the bump seed
-pub fn bump_seed(&self) -> u8 {
-    self.bump_seed
-}
-
-
-#[derive(Clone, BorshSerialize, BorshDeserialize)]
-pub struct InitializeDataStoreArgs {
-    pub authority: Pubkey,
-    pub space: u64,
-    pub is_dynamic: bool,
-    pub is_created: bool,
-    pub debug: bool,
-}
-
-#[derive(Clone, BorshSerialize, BorshDeserialize)]
-pub struct UpdateDataStoreArgs {
-    pub data_type: DataStoreTypeOption,
-    pub data: Vec<u8>,
-    pub offset: u64,
-    pub verify_data: bool,
-    pub realloc_down: bool,
-    pub debug: bool
-}
-
-#[derive(Clone, BorshDeserialize, BorshSerialize)]
-pub struct CloseDataStoreArgs {
-    pub debug: bool
+    pub fn bump_seed(&self) -> u8 {
+        self.bump_seed
+    }
 }
